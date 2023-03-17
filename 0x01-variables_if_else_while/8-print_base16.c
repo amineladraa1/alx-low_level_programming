6cd41cfadf1a9cabda7f7fde0cc69c2ef8fa5d8c
Amine <amineladraa67@gmail.com>
2023-03-17 10:41:37 -0700

    8. Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char n_ch;
+	char a_ch;
+
+	n_ch = '0';
+	a_ch = 'a';
+
+	while (n_ch <= 57)
+	{
+		putchar(n_ch);
+		n_ch++;
+	}
+	while (a_ch <= 122 && a_ch != 'g')
+        {
+                putchar(a_ch);
+                a_ch++;
+        }
+	putchar('\n');
+	return (0);
+}
