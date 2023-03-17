6529b1ae1dec0215369ddb2b07a9f5b649ef0bf1
Amine <amineladraa67@gmail.com>
2023-03-17 09:44:19 -0700

    5. Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -10,13 +10,12 @@
 
 int main(void)
 {
-	int i;
+	char i;
 
-	i = 0;
-	for (i = 0 ; i < 10 ; i++)
-	{
-		putchar('%d', i);
-	}
+	i = '0';
+	for (i = '0' ; i < '10' ; i++)
+		putchar(i);
+
 	putchar('\n');
 	return (0);
 }
