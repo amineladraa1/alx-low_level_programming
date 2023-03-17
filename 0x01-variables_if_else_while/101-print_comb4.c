7b8f69708794176fe08ac22cc9caeb2d5f12a08e
Amine <amineladraa67@gmail.com>
2023-03-17 13:48:56 -0700

    11. The success combination in business is: Do what you do better... and: do more of what you do...

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+	
+	for (i = 48 ; i <= 57 ; i ++)
+	{
+		for (j = i+1 ; j <= 57 ; j++)
+		{
+			for (k = j+1 ; k <= 57 ; k++)
+			{
+				if (i != j != k)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if (i + j + k != 168)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}	
+			}	
+		}	
+	}
+	putchar('\n');
+	return (0);
+}
