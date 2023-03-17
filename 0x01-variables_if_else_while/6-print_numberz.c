ef9261a5665dc0da07daa0333f8761b6f2a323b9
Amine <amineladraa67@gmail.com>
2023-03-17 10:08:00 -0700

    6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+
+	for (i = 0 ; i <= 9 ; i++)
+		putchar(i);
+
+	putchar('\n');
+	return (0);
+}
