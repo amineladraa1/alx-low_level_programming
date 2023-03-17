00f7b7a5ce45a8161792539983c470d20f9da7f2
Amine <amineladraa67@gmail.com>
2023-03-17 08:12:24 -0700

    3. alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+        char ch;
+	char Uch;
+
+        ch = 'a';
+	Uch = 'A';
+        
+	while (ch <= 122)
+        {
+                putchar(ch);
+                ch++;
+        }
+	while (Uch <= 90)
+        {
+                putchar(Uch);
+                Uch++;
+        }
+        putchar('\n');
+        return (0);
+}
