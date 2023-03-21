7d435d7ba5307ad9d6189cba35d085fa3df53f9c
Amine <amineladraa67@gmail.com>
2023-03-21 13:33:39 -0700

    9. Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,20 +8,21 @@
 
 void times_table(void)
 {
-	int i , j , multi;
-	for ( i = 0 ; i < 10 ; i++)
+	int i, j, multi;
+
+	for (i = 0 ; i < 10 ; i++)
 	{
-	for ( j = 0 ; j < 10 ; j++)
+	for (j = 0 ; j < 10 ; j++)
 	{
 		multi = i * j;
-		if ( multi >= 10 )
+		if (multi >= 10)
 		{
 			_putchar(' ');
 			_putchar((multi / 10) + '0');
 			_putchar((multi % 10) + '0');
-			if (j != 9 )
+			if (j != 9)
 				_putchar(',');
-		}else
+		} else
 		{
 		if (j != 0)
 		{
@@ -29,7 +30,7 @@ void times_table(void)
 			_putchar(' ');
 		}
 			_putchar(multi + '0');
-			if (j != 9 )
+			if (j != 9)
 				_putchar(',');
 		}
 	}
