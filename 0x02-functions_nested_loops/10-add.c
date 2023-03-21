dc172dc3a1deee74efc62a2f5f61bd3ee9c9f94c
Amine <amineladraa67@gmail.com>
2023-03-21 13:45:35 -0700

    10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ * Description: 'the program's description'
+ * @n1: number to check
+ * @n2: number
+ * Return: Always 0 (Success)
+ */
+
+int add(int a, int b)
+{
+	int res;
+
+	res = a + b;
+
+	return (res);
+}
