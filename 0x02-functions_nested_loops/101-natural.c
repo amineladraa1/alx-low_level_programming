18a1ad6820ac3b3de08bbb3e8e36cc28abec02c0
Amine <amineladraa67@gmail.com>
2023-03-21 16:27:17 -0700

    13. Nature made the natural numbers; All else is the work of women

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -9,14 +9,15 @@
  */
 int main(void)
 {
-	int i ,sum ;
-	
+	int i, sum;
+
 	sum = 0;
-	for( i = 0 ; i < 1024 ; i++)
+
+	for (i = 0 ; i < 1024 ; i++)
 	{
-		if(i % 3 == 0 || i % 5 == 0)
+		if (i % 3 == 0 || i % 5 == 0)
 			sum += i;
 	}
-	printf("%d\n",sum);
+	printf("%d\n", sum);
 	return (0);
 }
