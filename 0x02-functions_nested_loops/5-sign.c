d85472612de1abe995e5409890c4ab3b3329b124
Amine <amineladraa67@gmail.com>
2023-03-21 10:30:43 -0700

    5. Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -17,7 +17,7 @@ int print_sign(int n)
 		_putchar('+');
 		return (1);
 
-	}else if (n == 0)
+	} else if (n == 0)
 	{
 		_putchar('0');
 		return (0);
@@ -26,5 +26,4 @@ int print_sign(int n)
 
 	_putchar('-');
 	return (-1);
-	
 }
