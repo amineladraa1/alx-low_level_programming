e83f2a8bc6e8b6dda04ef2644dc9dc8b07687457
Amine <amineladraa67@gmail.com>
2023-03-21 08:35:33 -0700

    1. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include 'main.h'
+
+/**
+ * _islower - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: if c is lowercase 1 , else return 0  (Success)
+ */
+
+int _islower(int c)
+{
+	if( c >= 97 && c <= 122 )
+		return 1;
+	return 0;
+}
