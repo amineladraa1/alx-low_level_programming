bc829f0f64d60025182ea46980c76f880e120249
Amine <amineladraa67@gmail.com>
2023-03-21 14:06:16 -0700

    11. 98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * @n: number to check
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_to_98(int n)
+{
+	if (n < 99)
+	{
+		while (n < 99)
+		{
+			printf("%d, ",n);
+			n++;
+		}
+	}else if (n >= 98)
+	{
+		while(n >= 98)
+		{
+			printf("%d, ",n);
+			n--;
+		}
+	}
+}
