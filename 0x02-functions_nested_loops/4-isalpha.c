ef7ff7b311c6863fea0dc9d48ae958961bffda0c
Amine <amineladraa67@gmail.com>
2023-03-21 10:13:53 -0700

    4. isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isalpha - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * @c : the character to check
+ *
+ * Return:  0 if it's alphabet 1 if it's not
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122)
+		      return (1);	
+	return (0);
+}
