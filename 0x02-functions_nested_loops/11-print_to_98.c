387fbf2b5df10f9f134b79ea5b1b6915f825f1fa
Amine <amineladraa67@gmail.com>
2023-03-21 14:10:25 -0700

    11. 98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -17,14 +17,18 @@ void print_to_98(int n)
 	{
 		while (n < 99)
 		{
-			printf("%d, ",n);
+			if (n != 98)
+				printf("%d, ",n);
+			printf("%d",n);
 			n++;
 		}
 	}else if (n >= 98)
 	{
 		while(n >= 98)
 		{
-			printf("%d, ",n);
+			if (n != 98)
+				printf("%d, ",n);
+			printf("%d",n);
 			n--;
 		}
 	}
