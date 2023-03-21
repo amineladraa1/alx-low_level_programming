1dba0381339b6370a719e330f6a1ea4522508d2e
Amine <amineladraa67@gmail.com>
2023-03-21 12:33:34 -0700

    8. I'm federal agent Jack Bauer, and today is the longest day of my life

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,9 +8,11 @@
 
 void jack_bauer(void)
 {
-	int i , j ;
-	for (i = 0 ; i < 24 ; i++){
-		for (j = 0 ; j < 60 ; j++){
+	int i, j;
+	for (i = 0 ; i < 24 ; i++)
+	{
+		for (j = 0 ; j < 60 ; j++)
+		{
 			if (i < 10 && j < 10)
 			{
 				_putchar(0 + '0');
@@ -35,13 +37,15 @@ void jack_bauer(void)
 				_putchar(0 + '0');
 				_putchar(j + '0');
 				_putchar('\n');
-			}
+			}else if (i >= 10 && j >= 10)
+			{
 			_putchar((i / 10) + '0');
 			_putchar((i % 10) + '0');
 			_putchar(':');
 			_putchar((j / 10) + '0');
 			_putchar((j % 10) + '0');
 			_putchar('\n');
+			}
 		}
 	}
 }
