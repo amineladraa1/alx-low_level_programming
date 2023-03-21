9dfc36b789f27ade0a08c088fce7626f6e697992
Amine <amineladraa67@gmail.com>
2023-03-21 12:30:40 -0700

    8. I'm federal agent Jack Bauer, and today is the longest day of my life

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,23 +11,24 @@ void jack_bauer(void)
 	int i , j ;
 	for (i = 0 ; i < 24 ; i++){
 		for (j = 0 ; j < 60 ; j++){
-			if (i < 10 && j < 10){
+			if (i < 10 && j < 10)
+			{
 				_putchar(0 + '0');
 				_putchar(i + '0');
 				_putchar(':');
 				_putchar(0 + '0');
 				_putchar(j + '0');
 				_putchar('\n');
-			}
-			if (i < 10 && j >= 10){
+			}else if (i < 10 && j >= 10)
+			{
 				_putchar(0 + '0');
 				_putchar(i + '0');
 				_putchar(':');
 				_putchar((j / 10) + '0');
 				_putchar((j % 10) + '0');
 				_putchar('\n');
-			}
-			if (i >= 10 && j < 10){
+			}else if (i >= 10 && j < 10)
+			{
 				_putchar((i / 10) + '0');
 				_putchar((i % 10) + '0');
 				_putchar(':');
@@ -35,14 +36,12 @@ void jack_bauer(void)
 				_putchar(j + '0');
 				_putchar('\n');
 			}
-			if (i >= 10 && j >= 10){
-				_putchar((i / 10) + '0');
-				_putchar((i % 10) + '0');
-				_putchar(':');
-				_putchar((j / 10) + '0');
-				_putchar((j % 10) + '0');
-				_putchar('\n');
-			}
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(':');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			_putchar('\n');
 		}
 	}
 }
