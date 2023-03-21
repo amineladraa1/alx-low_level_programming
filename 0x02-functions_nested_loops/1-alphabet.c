0ff369b117582cbb88c9d2a2e09913ef58f1cc67
Amine <amineladraa67@gmail.com>
2023-03-21 06:59:45 -0700

    1. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+
+
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _putchar(char c);
+
+int main(void)
+{
+	int i;
+	i = 48;
+	for (i = 48 ; i < 57 ; i++ )
+	{
+		_putchar(i);
+	}
+	_putchar("\n");
+	return (0);
+}
