a1de07d45211c6b7b21597cea2a96222089069f3
Amine <amineladraa67@gmail.com>
2023-03-23 10:07:45 -0700

    5. Numbers constitute the only universal language

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ *
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+	char str[] = "01234567891011121314";
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j <= 19; j++)
+		{
+			_putchar(str[j]);
+		}
+	_putchar('\n');
+	}
+	_putchar('\n');
+}
