f059c3bda9ac0ba647b289a1a064c060cbfc427f
Amine <amineladraa67@gmail.com>
2023-03-23 08:28:31 -0700

    1. isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isdigit - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * @c: number to check
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	return (0);	
+}
