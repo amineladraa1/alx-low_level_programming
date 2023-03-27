c3db67b0eb4791cd9ef18e1f986a7459a07867c2
Amine <amineladraa67@gmail.com>
2023-03-27 11:32:16 -0700

    6. Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * puts2 - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * @str: number to check
+ *
+ * Return: void (Success)
+ */
+
+void puts2(char *str)
+{
+	int length = 0;
+	int i;
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+	for (i = 0 ;i < length ;i++)
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i];
+		}
+	}
+	_putchar('\n');
+}
