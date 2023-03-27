d793acf8c81ef6540bb9aeeb46d11a07e9b87c48
Amine <amineladraa67@gmail.com>
2023-03-27 08:24:40 -0700

    0. 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * @*n: number to check
+ *
+ * Return: void (Success)
+ */
+
+void reset_to_98(int *n)
+{
+	n = 98;
+}
