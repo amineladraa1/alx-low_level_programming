2f927b516e0b3d89824d1ee571e280d8ab7e2a5e
Amine <amineladraa67@gmail.com>
2023-03-29 09:19:27 -0700

    3. strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -17,10 +17,10 @@ int _strcmp(char *s1, char *s2)
 	{
 		if (*s1 != *s2)
 		{
-			return (*s1 < *s2) ? -15 : 15;
+			return ((*s1 < *s2) ? -1 : 1);
 		}
 		s1++;
 		s2++;
 	}
-	return 0;
+	return (0);
 }
