9dafd5d5198d6be83e09d75dfb50669b68e4ee73
Amine <amineladraa67@gmail.com>
2023-03-29 11:14:14 -0700

    5. Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * string_toupper - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * @str: number to check
+ *
+ * Return: Always 0 (Success)
+ */
+
+char *string_toupper(char *str)
+{
+	char *p = str;
+	while (*p)
+	{
+		if (*p >= 'a' && *p <= 'z')
+		{
+		*p = *p - 'a' + 'A';
+		}
+	p++;
+	}
+	return (str);
+}
