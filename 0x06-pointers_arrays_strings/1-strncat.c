e1955fc299da251c77603358abd2f6b1d3ea2c07
Amine <amineladraa67@gmail.com>
2023-03-29 08:23:55 -0700

    1. strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strncat - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * @dest: char to paste to
+ * @src: char to copy
+ * Return: a pointer dest (Success)
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	char *p = dest;
+
+	while (*p != '\0')
+	{
+		p++;
+	}
+	int i = 0;
+
+	while (*src != '\0' && i < n)
+	{
+	*p++ = *src++;
+	i++;
+	}
+	*p = '\0';
+	return dest;
+}
