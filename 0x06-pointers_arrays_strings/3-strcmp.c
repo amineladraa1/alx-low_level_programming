99623627a473964a149ef60f0412d71d8da57112
Amine <amineladraa67@gmail.com>
2023-03-29 09:17:43 -0700

    3. strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strcmp - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * @s1: char to check
+ * @s2: char to check
+ *
+ * Return: 0 if aqual 1 if s1 is bigger 2 if s2 is bigger (Success)
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 != '\0' || *s2 != '\0')
+	{
+		if (*s1 != *s2)
+		{
+			return (*s1 < *s2) ? -15 : 15;
+		}
+		s1++;
+		s2++;
+	}
+	return 0;
+}
