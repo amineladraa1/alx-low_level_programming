71d42606a1256483a1020eae02cf53e35cb9ef30
Amine <amineladraa67@gmail.com>
2023-03-30 07:07:02 -0700

    8. rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -13,17 +13,9 @@
 char *rot13(char *str)
 {
 	char *p = str;
-	while (*p)
-	{
-	if ((*p >= 'A' && *p <= 'M') || (*p >= 'a' && *p <= 'm'))
-	{
-		*p += 13;
-	}
-	else if ((*p >= 'N' && *p <= 'Z') || (*p >= 'n' && *p <= 'z'))
-	{
-		*p -= 13;
-	}
-	p++;
-	}
-	return (str);
+    while (*p) {
+        *p += ((*p >= 'A' && *p <= 'Z') || (*p >= 'a' && *p <= 'z')) * 13 - ((*p >= 'N' && *p <= 'Z') || (*p >= 'n' && *p <= 'z')) * 26;
+        p++;
+    }
+    return str;
 }
