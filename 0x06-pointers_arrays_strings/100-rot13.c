965a6ac228f455daf4c5664710c949662a967cbb
Amine <amineladraa67@gmail.com>
2023-03-30 07:23:49 -0700

    8. rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -12,21 +12,26 @@
 
 char *rot13(char *str)
 {
-	 char letters[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    char rot13[]  =  "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-    
-    char *result = str;
-    while (*str != '\0') {
-        char c = *str;
-        int i;
-        for (i = 0; i < 52; i++) {
-            if (c == letters[i]) {
-                c = rot13[i];
-                break;
-            }
-        }
-        *str = c;
-        str++;
-    }
-    return result;
+	char letters[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot13[]  =  "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	char *result = str;
+
+
+	while (*str != '\0')
+	{
+	char c = *str;
+	int i;
+
+	for (i = 0; i < 52; i++)
+	{
+		if (c == letters[i])
+		{
+		c = rot13[i];
+		break;
+		}
+	}
+	*str = c;
+	str++;
+	}
+	return (result);
 }
