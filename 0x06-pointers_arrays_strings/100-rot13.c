9696db3120be5ebb53555c0070632d460fdcebdd
Amine <amineladraa67@gmail.com>
2023-03-30 06:56:45 -0700

    8. rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rot13 - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * @str: char to check
+ *
+ * Return: pointer to a string (Success)
+ */
+
+char *rot13(char *str)
+{
+	char *p = str;
+	while (*p)
+	{
+	if ((*p >= 'A' && *p <= 'M') || (*p >= 'a' && *p <= 'm'))
+		*p += 13;
+	else if ((*p >= 'N' && *p <= 'Z') || (*p >= 'n' && *p <= 'z'))
+		*p -= 13;
+	p++;
+	}
+	return (str);
+}
