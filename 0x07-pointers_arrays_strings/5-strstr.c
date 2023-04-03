e73b5a295838ac001d02287fe1c2d534fc1af7e9
Amine <amineladraa67@gmail.com>
2023-04-03 10:31:08 -0700

    5. strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strstr - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * @haystack: string to check
+ * @needle: string to find
+ *
+ * Return: pointer to occurence or null (Success)
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	char *hay;
+	while (*haystack)
+	{
+		hay = haystack;
+
+		while (*needle && *needle == *hay)
+		{
+			hay++;
+			needle++;
+		}
+		if (*needle == '\0')
+		{
+			return (haystack);
+		}
+		haystack++;
+	}
+	return (0);
+}
