e5b71d222ad381e324857f658b264eb099bf4a50
Amine <amineladraa67@gmail.com>
2023-04-03 08:43:10 -0700

    4. strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strpbrk - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * @s: string to check
+ * @accept: string to xeck from
+ *
+ * Return: pointer to first occurence (Success)
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	while (*s != '\0')
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+			{
+				return (s);
+			}
+		}
+		s++;
+	}
+	return (0);
+}
