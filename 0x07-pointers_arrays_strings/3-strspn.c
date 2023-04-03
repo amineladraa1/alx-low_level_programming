860342283759aedd64be231660432da3818cc694
Amine <amineladraa67@gmail.com>
2023-04-03 08:28:25 -0700

    3. strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strspn - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * @s: string to check
+ * @accept: string to xeck from
+ *
+ * Return: unsigned int of length (Success)
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int length;
+	int i;
+	int fflag = 1;
+
+	while (*s != '\0' && fflag)
+	{
+		fflag = 0;
+
+		for (i = 0; accept[i]; i++)
+		{
+			if(*s == accept[i])
+			{
+				fflag = 1;
+				length++;
+				s++;
+			}
+		}
+	}
+}
