f2ac36de9377d7ff2eda92cb33dcaf2d4126f90b
Amine <amineladraa67@gmail.com>
2023-04-03 07:21:00 -0700

    2. strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strchr - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * @s: string to check
+ * @c: character to follow
+ *
+ * Return: pointer to first occurrence (Success)
+ */
+
+char *_strchr(char *s, char c)
+{
+	char *p;
+
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			p = s;
+			break;
+		}
+		s++;
+	}
+	return (p);
+}
