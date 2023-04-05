651ddeef4a35255c59d0fec12c5134b632e069ca
Amine <amineladraa67@gmail.com>
2023-04-05 11:09:15 -0700

    6. Inception. Is it possible?

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * is_prime_number - Entry point
+ *
+ * Description: 'the program check if a number is prime'
+ *
+ * @n: number to check
+ *
+ * Return: 0 or 1 if it's a prime  (Success)
+ */
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	return (_prime_h(n, n - 1));
+}
+
+/**
+ * _prime_h - Entry point
+ *
+ * Description: 'helper function'
+ *
+ * @n: number to check
+ * @devisor: to devide n
+ *
+ * Return: 0 (failed) or 1 (Success)
+ */
+
+int _prime_h(int n, int devisor)
+{
+	if (devisor == 1)
+	{
+		return (1);
+	}
+	if (n % devisor == 0)
+	{
+		return (0);
+	}
+	return (n, devisor - 1);
+}
