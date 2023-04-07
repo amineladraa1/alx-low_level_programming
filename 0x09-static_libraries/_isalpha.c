f4542a373172a2d7eb695e036ad99f7a07c9d098
Amine <amineladraa67@gmail.com>
2023-04-07 09:36:08 -0700

    0. A library is not a luxury but one of the necessities of life

---
+++ 0x09-static_libraries/_isalpha.c
@@ -0,0 +1,8 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+
+int _isalpha(int c)
+{
+	return isalpha(c);
+}
---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,8 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+int _putchar(char c)
+{
+	return putchar(c);
+}
