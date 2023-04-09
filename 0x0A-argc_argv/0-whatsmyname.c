f297851da6e6993b77de48bf9f750a97f13c1721
Amine <amineladraa67@gmail.com>
2023-04-09 07:45:30 -0700

    1. Silence is argument carried out by other means

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,16 +2,17 @@
 
 /**
  * main - prints the name of the program
- * 
+ *
  * @argc: number of arguments
  * @argv: array of arguments
  *
  * Return: Always 0 (Success)
  */
 
-int main(int argc __attribute__((unused)), char* argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s", argv[0]);
+
 
 	return (0);
 }
