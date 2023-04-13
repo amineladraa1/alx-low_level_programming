32cd341cf90a3ab9bddf0934f6e379a335748a0f
Amine <amineladraa67@gmail.com>
2023-04-13 09:00:34 -0700

    1. string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -9,16 +9,13 @@
 * @s1: first char
 * @s2: secound char
 * @n: unsigned int
-*
 * Return: If the function fails, it should return NULL
 */
-
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int x, y, z;
 	char *s;
-
 
 	if (s1 == NULL)
 	{
@@ -27,9 +24,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	else
 	{
 		for (x = 0; s1[x]; ++x)
-		{
 			;
-		}
 	}
 	if (s2 == NULL)
 	{
@@ -38,20 +33,15 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	else
 	{
 		for (y = 0; s2[y]; ++y)
-		{
 			;
-		}
 	}
 	if (y > n)
 	{
 		y = n;
 	}
 	s = malloc(sizeof(char) * (x + y + 1));
-
 	if (s == NULL)
-	{
 		return (NULL);
-	}
 	for (z = 0; z < x; z++)
 	{
 		s[z] = s1[z];
@@ -60,9 +50,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		s[z + x] = s2[z];
 	}
-
 	s[x + y] = '\0';
-
 	return (s);
 }
-
