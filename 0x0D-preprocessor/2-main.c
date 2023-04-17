22449d718acea24c724c43a60eebee0621a44eec
Amine <amineladraa67@gmail.com>
2023-04-17 11:06:18 -0700

    2. File name

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: 'the program return the file name'
+ *
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
-	printf("%s", __FILE__);
+	printf("%s\n", __FILE__);
 	return (0);
 }
