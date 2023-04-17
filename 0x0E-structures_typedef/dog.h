251d4c5457ca63b247184316c44df2b5475f3115
Amine <amineladraa67@gmail.com>
2023-04-17 06:53:30 -0700

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * struct dog - Dog type definition
+ * @name: First member
+ * @age: Second member
+ * @owner: third
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
