b099e596752c7033a2d529b548bc1ee6bb8f002b
Amine <amineladraa67@gmail.com>
2023-04-20 14:37:58 -0700

    2. One woman's constant is another woman's variable

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_strings - Entry point
+ *
+ * Description: 'the program's print strings'
+ *
+ * @n: number of args
+ * @separator: sepa string
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list args;
+	char *str;
+
+	if (separator == NULL)
+		return;
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(args, char *);
+		if (str == NULL)
+			printf("(nil)");
+		(i == n - 1)
+		? printf("%s",str)
+		: printf("%s%s",str ,separator);
+	}
+	printf("\n");
+	va_end(args);
+}
