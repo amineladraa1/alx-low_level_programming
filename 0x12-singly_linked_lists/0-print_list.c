927e08fc03606a196ce23921c187291574efcaca
amineladraa1 <amineladraa1@gmail.com>
2023-04-27 07:10:32 -0700

    0. Print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+
+/**
+ * print_list - Entry point
+ *
+ * Description: 'prints the linked list and return num of nodes'
+ *
+ * @h: structure to print
+ *
+ * Return: number of nodes (Success)
+ */
+
+size_t print_list(const list_t *h)
+{
+	if (h->str == NULL)
+	{
+		printf("[%d] %s\n",0,"(nil)");
+	}
+	else
+	{
+		printf("[%d] %s\n",h->len, h->str);
+	}
+	if (h->next == NULL)
+		return (1);
+	return ((print_list(h->next) + 1));
+}
