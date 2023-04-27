7e61b4937e5cacc5f9e4ebcf14c2e8a4ef10b6ff
amineladraa1 <amineladraa1@gmail.com>
2023-04-27 08:17:18 -0700

    1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * list_len - Entry point
+ *
+ * @h: pointer to the list_t list
+ *
+ * Return: number of elements in h
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t counter = 0;
+
+	while (h != NULL)
+	{
+		counter++;
+		h = h->next;
+	}
+	return (counter);
+}
