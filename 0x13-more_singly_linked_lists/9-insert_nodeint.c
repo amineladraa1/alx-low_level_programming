a9b765574a2e7efc04ce8944b1e4a7b5e244c3cc
amineladraa1 <amineladraa1@gmail.com>
2023-04-29 13:13:14 -0700

    9. Insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -45,7 +45,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		currentNode->next = newNode;
 		return (newNode);
 	}
-	if (!currentNode && index == idx)
+	if (currentNode && index == idx)
 	{
 		prevNode->next = newNode;
 		newNode->next = currentNode;
