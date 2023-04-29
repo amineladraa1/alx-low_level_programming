4f2be102db9a245298ccd6dae6ac304aba0bfc74
amineladraa1 <amineladraa1@gmail.com>
2023-04-29 13:31:50 -0700

    9. Insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,7 +17,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int index = 0;
 	listint_t *currentNode = *head;
-	listint_t *prevNode = NULL;
 	listint_t *newNode;
 
 	newNode = malloc(sizeof(listint_t));
