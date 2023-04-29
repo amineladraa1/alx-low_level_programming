64add32d26d674ccf4717079d538f3e699017e95
amineladraa1 <amineladraa1@gmail.com>
2023-04-29 05:22:16 -0700

    0. Print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -15,10 +15,13 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t counter;
+	size_t counter = 0;
 
 	if (h == NULL)
-		return (-1);
+	{
+		printf("Error\n");
+        	return (1);
+	}
 	while (h)
 	{
 	printf("%d\n", h->n);
