b86634181733d2d2ad369f49c7a257d44938b01f
amineladraa1 <amineladraa1@gmail.com>
2023-04-29 08:58:07 -0700

    8. Sum list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,25 @@
+#include <string.h>
+#include "lists.h"
+#include <stdlib.h>
+
+
+/**
+ * sum_listint - Entry point
+ * @head: first node in the linked list
+ *
+ * Return: resulting sum
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+	listint_t *currentNode = *node;
+
+	if (!head)
+		return (0);
+	while (currentNode)
+	{
+		sum += currentNode->n;
+		currentNode = currentNode->next;
+	}
+	return (sum);
+}
