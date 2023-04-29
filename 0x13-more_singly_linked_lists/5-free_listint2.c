35be36ed07dbdcf3aaec89422eaf8453ad5285e8
amineladraa1 <amineladraa1@gmail.com>
2023-04-29 06:32:39 -0700

    5. Free

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - Entry point
+ *
+ * Description : free all the lists
+ *
+ * @head: list_t list to be freed
+ *
+ * return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *node;
+	
+	if (head == NULL)
+		return;
+	while (head)
+	{
+		node = head->next;
+		free (head);
+		head = node;
+	}
+	*head = NULL
+}
