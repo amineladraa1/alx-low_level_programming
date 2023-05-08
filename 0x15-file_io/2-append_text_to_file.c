5ed4ce857c1d0448f30255fd372182d43d5099d8
amineladraa1 <amineladraa1@gmail.com>
2023-05-08 08:24:53 -0700

    2. Speak gently, she can hear

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * append_text_to_file -Entry point.
+ *
+ * @filename: A pointer to the name of the file.
+ * @text_content: The string to add to the end of the file.
+ *
+ * Return: If the function fails or filename is NULL - -1.
+ *         If the file does not exist the user lacks write permissions - -1.
+ *         Otherwise - 1.
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int des, written, length;
+
+	if (text_content == NULL)
+		return (-1);
+	length = strlen(text_content);
+	des = open(filename, O_WRONLY | O_APPEND);
+	written = write(des, text_content, length);
+
+	if (des == -1 || written == -1)
+		return (-1);
+	close(des);
+	return (1);
+}
