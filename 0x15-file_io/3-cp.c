eb1647a73a945c691cf2de99f5d4f5cdcc389c9a
amineladraa1 <amineladraa1@gmail.com>
2023-05-08 10:30:48 -0700

    3. cp

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,96 @@
+#include "main.h"
+
+
+
+/**
+ * create_buffer - Entry point
+ *
+ * Description : Allocates 1024 bytes for a buffer.
+ *
+ * @file: The name of the file buffer is storing chars for.
+ *
+ * Return: A pointer to the newly-allocated buffer.
+ */
+
+char *create_buffer(char *file)
+{
+	char *buffer;
+
+	buffer = malloc(sizeof(char) * 1024);
+
+	if (buffer == NULL)
+	{
+		dprintf(STDERR_FILENO,
+			"Error: Can't write to %s\n", file);
+		exit(99);
+	}
+
+	return (buffer);
+}
+
+/**
+ * close_file - Entry point
+ * Description: Closes file descriptors.
+ * @fd: The file descriptor to be closed.
+ */
+
+void close_file(int fd)
+{
+	int c;
+
+	c = close(fd);
+
+	if (c == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
+/**
+ * main - Entry point
+ *
+ * Description: the program's return 0
+ *
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int read, written, file_from, file_to;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n";
+		return (97);
+	}
+	buffer = create_buffer(argv[2]);
+	file_from = open(argv[1], O_RDONLY);
+	file_to = open(argv[2], O_CREATE | O_WRONLY | O_TRUNC, 0664);
+	read = read(file_from, buffer, 1024);
+
+	while (read > 0)
+	{
+		if (file_from == -1 || read == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s", argv[1]);
+			return (98);
+		}
+		written = write(file_to, buffer, 1024);
+
+		if (file_to == -1 || written == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			free(buffer);
+			exit(99);
+		}
+		read = read(file_from, buffer, 1024);
+		file_to = open(argv[2], O_WRONLY | O_APPEND);
+	}
+	free(buffer);
+	close_file(file_from);
+	close_file(file_to);
+	return (0);
+}
