7487e5524d28b7aca6bc2b5c3f56c9d6354fe1e4
amineladraa1 <amineladraa67@gmail.com>
2023-07-10 04:41:42 -0700

    0. Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,32 +15,33 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char *buffer;
 	ssize_t read_bytes;
 	ssize_t write_bytes;
-	int desc;
+	int descriptor;
 
-	if (filename = NULL)
+	if (filename == NULL)
 		return (0);
 
-	desc = open(filename, O_RDONLY);
+	descriptor = open(filename, O_RDONLY);
 
-	if (desc == -1)
+	if (descriptor == -1)
 		return (0);
+
 	buffer = (char *)malloc(sizeof(char) * letters);
 
-	if (buffer = NULL)
+	if (buffer == NULL)
 	{
-		close(desc);
+		close(descriptor);
 		return (0);
 	}
-	read_bytes = read(desc, buffer, letters);
+	read_bytes = read(descriptor, buffer, letters);
 	write_bytes = write(STDOUT_FILENO, buffer, read_bytes);
 
 	if (write_bytes == -1 || read_bytes == -1)
 	{
-		close(desc);
+		close(descriptor);
 		free(buffer);
 		return (0);
 	}
-	close(desc);
+	close(descriptor);
 	free(buffer);
 	return (write_bytes);
 }
