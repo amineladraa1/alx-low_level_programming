1722adfea8c6c0aea5e495b896149537f2b3bbe0
amineladraa1 <amineladraa67@gmail.com>
2023-07-10 04:39:26 -0700

    0. Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,8 +1,5 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
 
 /**
  * read_textfile- Entry point.
@@ -18,32 +15,32 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char *buffer;
 	ssize_t read_bytes;
 	ssize_t write_bytes;
-	int descriptor;
+	int desc;
 
 	if (filename = NULL)
 		return (0);
 
-	descriptor = open(filename, O_RDONLY);
+	desc = open(filename, O_RDONLY);
 
-	if (descriptor == -1)
+	if (desc == -1)
 		return (0);
 	buffer = (char *)malloc(sizeof(char) * letters);
 
 	if (buffer = NULL)
 	{
-		close(descriptor);
+		close(desc);
 		return (0);
 	}
-	read_bytes = read(descriptor, buffer, letters);
+	read_bytes = read(desc, buffer, letters);
 	write_bytes = write(STDOUT_FILENO, buffer, read_bytes);
 
 	if (write_bytes == -1 || read_bytes == -1)
 	{
-		close(descriptor);
+		close(desc);
 		free(buffer);
 		return (0);
 	}
-	close(descriptor);
+	close(desc);
 	free(buffer);
-	return (0);
+	return (write_bytes);
 }
