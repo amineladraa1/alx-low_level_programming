e6764b768a7d17ba6899998c185bdc455b16e263
amineladraa1 <amineladraa67@gmail.com>
2023-08-17 07:49:48 -0700

    3. Add node at the end

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -13,10 +13,13 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	dlistint_t *current;
 
 	newNode = (dlistint_t *)malloc(sizeof(dlistint_t));
+
 	if (newNode == NULL)
 		return (NULL);
+
 	newNode->n = n;
 	newNode->next = NULL;
+	newNode->prev = NULL;
 
 	if (*head == NULL)
 	{
