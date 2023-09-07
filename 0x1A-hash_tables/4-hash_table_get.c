f79e972e9f4506672b2f8917d7241de8ee2d6b34
amineladraa1 <amineladraa67@gmail.com>
2023-09-07 11:11:38 -0700

    4. >>> ht['betty']

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,10 +1,10 @@
 #include "hash_tables.h"
 
 /**
- * hash_djb2 - implementation of the djb2 algorithm
- * @str: string used to generate hash value
- *
- * Return: hash value
+ * hash_table_get - retrieves a value associated with a key.
+ * @ht: hash table used
+ * @key: key
+ * Return: value or NULL
  */
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
@@ -12,10 +12,10 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 
 	index = key_index((unsigned char *)key, ht->size);
-	if (ht->array[index])
-		return (ht->array[index]->value);
-	else
-	{
+
+	if (ht == NULL || key == NULL || *key == '\0')
 		return (NULL);
-	}
+	if (!ht->array[index])
+		return (NULL);
+	return (ht->array[index]->value);
 }
