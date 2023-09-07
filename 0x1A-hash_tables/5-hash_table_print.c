007ec7c05aa5f42393c52783dce8455046070adc
amineladraa1 <amineladraa67@gmail.com>
2023-09-07 11:49:31 -0700

    5. >>> print(ht)

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,35 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints a hash table
+ * @ht: hash table
+ *
+ * Return: void
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *current;
+	int first;
+
+	if (!ht)
+		return;
+	first = 1;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		current = ht->array[i];
+
+		while (current)
+		{
+			if (!first)
+				printf(", ");
+			printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
+			first = 0;
+			current = current->next;
+		}
+	}
+	printf("}\n");
+}
