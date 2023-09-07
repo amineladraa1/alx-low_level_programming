e539160d8b775b160dc292fb445054816f979b8f
amineladraa1 <amineladraa67@gmail.com>
2023-09-07 11:17:42 -0700

    4. >>> ht['betty']

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -10,12 +10,19 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index;
+	hash_node_t *current;
 
 	index = key_index((unsigned char *)key, ht->size);
 
 	if (ht == NULL || key == NULL || *key == '\0')
 		return (NULL);
-	if (!ht->array[index])
-		return (NULL);
-	return (ht->array[index]->value);
+	current = ht->array[index];
+
+	while (current)
+	{
+		if (strcmp(current->key, key) == 0)
+			return (current->value);
+		current = current->next;
+	}
+	return (NULL);
 }
