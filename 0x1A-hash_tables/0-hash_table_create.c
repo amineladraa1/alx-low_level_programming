31bd24f8e2d8651677f336c7d463d4adaa5335cc
amineladraa1 <amineladraa67@gmail.com>
2023-09-08 06:18:40 -0700

    0 task updated

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -14,18 +14,15 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	h_table	= malloc(sizeof(hash_table_t));
 
-	if (!h_table)
-	{
+	if (h_table == NULL)
 		return (NULL);
-	}
+
+	h_table->size = size;
 	h_table->array = malloc(sizeof(hash_node_t *) * size);
 
 	if (!h_table->array)
-	{
-		free(h_table);
 		return (NULL);
-	}
-	h_table->size = size;
+
 	for (i = 0; i < size; i++)
 		h_table->array[i] = NULL;
 
